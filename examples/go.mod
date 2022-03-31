1791a3d2f220a40397a549140151fe9e666035b8
Sagleft <sagleft@mfc.click>
2022-03-31 23:25:34 +0300

    update example

---
+++ examples/go.mod
@@ -0,0 +1,7 @@
+module test
+
+go 1.16
+
+require (
+    github.com/sagleft/go-nvs v1.0.0
+)
--- examples/write.go
+++ examples/write.go
@@ -4,16 +4,22 @@ import (
 	"log"
 	"os"
 
-	gonvs ".."
+	gonvs "github.com/sagleft/go-nvs"
 )
 
 func main() {
-	client := gonvs.NewClient(gonvs.CreateClientTask{
+	client, err := gonvs.NewClient(gonvs.CreateClientTask{
 		RPCUser:     os.Getenv("USER"),
 		RPCPassword: os.Getenv("PASSWORD"),
 	})
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	err := client.Write(gonvs.WriteEntryTask{})
+	err = client.Write(gonvs.WriteEntryTask{
+		Name:  "test:0001",
+		Value: []byte("entry value"),
+	})
 	if err != nil {
 		log.Fatalln(err)
 	}
